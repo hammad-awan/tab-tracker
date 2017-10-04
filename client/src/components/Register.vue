190827cd2ac5e59c933a51c1db762cbe51994c18
Awan <AwanH@corp.root.nasd.com>
2017-10-04 14:40:55 -0400

    Set up the register page.

---
+++ client/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <label for="name">Email:</label>
+    <input type="email" name="email" placeholder="email" v-model="email" />
+    <br/>
+    <label for="password">Password:</label>
+    <input type="password" name="password" placeholder="password" v-model="password" />
+    <br/>
+    <button @click="register">Register</button>
+    <p>
+      {{message}}
+    </p>
+  </div>
+</template>
+
+<script>
+import authenticationService from '@/services/authenticationService'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      message: ''
+    }
+  },
+  methods: {
+    async register() {
+      const response = await authenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      if (response.data) {
+        this.message = response.data.message
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const app = express();
+const app = express()
 
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: 'Hello world!'
-    });
-});
+app.post('/api/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}. Your user was registered!`
+  })
+})
 
-app.listen(process.env.PORT || 8081, () => {
-    console.log('Server running...');
-});
+const port = process.env.PORT || 3030
+
+app.listen(port, () => {
+  console.log('Localhost server running on port ' + port + '...')
+})
